763ce0acee64dfa9723effc0ed1a1e2a10e98180
Blanca Hernández Laserna <hdzlaserna@gmail.com>
2016-03-01 14:09:52 +0100

    First version

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ app.get("/about",(req,res)=>{
 	fs.readFile('contactos.json','utf-8',(err,content)=>{
 		console.log("Data read");
 		contactos = JSON.parse(content);
-		res.write("<html><body>------Contacts-------<ul>");
+		res.write("<html><body>------Members-------<ul>");
 		contactos.forEach((contacto)=>{
 			res.write("<li>"+contacto.name+"</li>");
 		});
-		res.write("</ul>------------</body></html>");
+		res.write("</ul>------------------------------------------------------------</body></html>");
 		res.end();
 	});
 });
+
+app.get("/about/country-types",(req,res)=>{
+	fs.readFile('country-types.json','utf-8',(err,content)=>{
+		console.log("Data read");
+		country-types= JSON.parse(content);
+		res.write("<html><body> we can see the percentage of population listening music , besides the musical genre most listened<ul> ");
+		country-types.forEach((linea)=>{
+			res.write("<li>"+linea.country+" , "+linea.year+" , "+linea.percentage+" , "+linea.type+"</li>");
+		});
+		res.end();
+
+	});
+});
+
+
+
 
 app.listen(process.env.PORT);
 
