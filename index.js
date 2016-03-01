aa5649d741bf5dc349bab629102c92576cb62593
Blanca Hernández Laserna <hdzlaserna@gmail.com>
2016-03-01 14:26:28 +0100

    first version

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ app.get("/about",(req,res)=>{
 	});
 });
 
-//app.get("/about/country-types",(req,res)=>{
-	//fs.readFile('countrytypes.json','utf-8',(err,content)=>{
-	//	console.log("Data read");
-	//	country-types= JSON.parse(content);
-	//	res.write("<html><body> we can see the percentage of population listening music , besides the musical genre most listened <ul>");
-	//	country-types.forEach((linea)=>{
-	//		res.write("<li>"+linea.country+" , "+linea.year+" , "+linea.percentage+" , "+linea.type+"</li>");
-	//	});
-	//	res.write("</ul>--------------------------------------------------------------------------</body></html>")
-	//	res.end();
+app.get("/about/country-types",(req,res)=>{
+	fs.readFile('countrytypes.json','utf-8',(err,content)=>{
+		console.log("Data read");
+		countrytypes= JSON.parse(content);
+		res.write("<html><body> we can see the percentage of population listening music , besides the musical genre most listened <ul>");
+		countrytypes.forEach((linea)=>{
+			res.write("<li>"+linea.country+" , "+linea.year+" , "+linea.percentage+" , "+linea.type+"</li>");
+		});
+		res.write("</ul>--------------------------------------------------------------------------</body></html>");
+		res.end();
 
-	//});
-//});
+	});
+});
 
 
 
