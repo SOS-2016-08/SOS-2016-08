ff04e6b6c201dff6d7246e60be349ed21c67a69e
Candela Madero Garcia <candela046@gmail.com>
2016-03-01 15:21:31 +0100

    hola

--- index.js
+++ index.js
@@ -29,7 +29,22 @@ app.get("/about/country-types",(req,res)=>{
 	});
 });
 
+app.get("/about/social-situation",(req,res)=>{
+	fs.readFile('socialsituation.json','utf-8',(err,content)=>{
+		console.log("Data read");
+		socialsituation= JSON.parse(content);
+		res.write("<html><body> Here we see the percentages according to digital music sales or physical format <ul>");
+		socialsituation.forEach((linea)=>{
+			res.write("<li>"+linea.country+" , "+linea.year+" , "+linea.percentage+" , "+linea.type+"</li>");
+		});
+		res.write("</ul>--------------------------------------------------------------------------</body></html>");
+		res.end();
 
+
+		
+
+	});
+});
 
 
 app.listen(process.env.PORT);
