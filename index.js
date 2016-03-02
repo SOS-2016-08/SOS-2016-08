d8a91ecbeb71a0e2db8905c55d8a4826696e38a3
Candela Madero <candela046@gmail.com>
2016-03-02 18:32:56 +0100

    socialsituacion changes 2

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ app.get("/about/social-situation",(req,res)=>{
 		res.write("<td><strong> year</strong></td>")
 		res.write("<td><strong> sales</strong></td>")
 		res.write("<td><strong> digital</strong></td>")
-		res.write("<td><strong> noDigital</strong></td>")
+		res.write("<td><strong> noDigital</strong></td></tr></table>")
 		
 		socialsituation.forEach((linea)=>{
 			//res.write("<li>"+linea.country  +" , "+linea.year+" , "+linea.sales+" , "+linea.digital+", "+linea.nodigital+"</li>");
-			res.write("<li><tr><td>"+linea.country+"</td></tr></li></tr></table>")
-			//res.write("<li><tr><td>"+linea.year+"</td></tr></li>")
+			res.write("<li><tr><td>"+linea.country+"</td></tr>","<tr><td>"+linea.year+"</td></tr></li>")
+			//res.write("<li><tr><td>"+linea.country+"</td></tr></li>")
 		});
 		res.write("</ul>--------------------------------------------------------------------------</body></html>");
 		res.end();
