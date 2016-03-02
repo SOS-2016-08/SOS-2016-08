04f43a49b72926d7e235585d310973f0b5a972c4
Candela Madero <candela046@gmail.com>
2016-03-02 18:11:42 +0100

    socialsituacion changes 2

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.get("/about/social-situation",(req,res)=>{
 		
 		socialsituation.forEach((linea)=>{
 			res.write("<li>"+linea.country  +" , "+linea.year+" , "+linea.sales+" , "+linea.digital+", "+linea.nodigital+"</li>");
+			res.write("<tr><td>"+linea.country+"</td></tr>")
 		});
 		res.write("</ul>--------------------------------------------------------------------------</body></html>");
 		res.end();
