acca68e0bd5262d954b54f42698d289517c5b546
Candela Madero <candela046@gmail.com>
2016-03-02 12:40:55 +0100

    socialsituacion changes 2

--- index.js
+++ index.js
@@ -33,8 +33,9 @@ app.get("/about/social-situation",(req,res)=>{
 	fs.readFile('socialsituation.json','utf-8',(err,content)=>{
 		console.log("Data read");
 		socialsituation= JSON.parse(content);
-		res.write("<html><body> Here we see the percentages according to digital music sales or physical format <ul>");
 
+		res.write("<html><body> Here we see the percentages according to digital music sales or physical format <ul>");
+		res.write("<table> <tr> <td><strong> Country <strong></td></tr></table>")
 		
 		socialsituation.forEach((linea)=>{
 			res.write("<li>"+linea.country+" , "+linea.year+" , "+linea.sales+" , "+linea.digital+", "+linea.nodigital+"</li>");
