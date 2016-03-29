20bcf46b57bde7261824f9b99b718a17136f0087
Blanca Hernández Laserna <hdzlaserna@gmail.com>
2016-03-29 15:02:45 +0200

    entrega dia 29

--- controles/musicCTL.js
+++ controles/musicCTL.js
@@ -1,9 +1,9 @@
 var fs= require("fs");
 
-var musical=[ {name : "pepe"},  {name : "juan"}, {name : "javi"}];
+var musical=[ {country:"Spain"    ,year:"2010",  percentage:"88%",  type:"reggaeton" }];
 
 
-module.exports.getLoad = function(req,res){
+module.exports.getLoad = function(req,res){ //FUNCIONA
 	musical=[];
 	var mus= fs.readFileSync('countrytypes.json', 'utf8');
 	musical = JSON.parse(mus);
@@ -15,25 +15,44 @@ module.exports.getLoad = function(req,res){
 
 
 
-module.exports.getMusic = function(req,res){
+module.exports.getMusic = function(req,res){// FUNCIONA
 	console.log("New Get of music ");
 	res.status(200).jsonp(musical);
 	
 };
 
+module.exports.getMusic3 = function(req,res){ //FUNCIONA
 
-
-module.exports.getMusic2 = function(req,res){
-	var name =req.params.name;
-	var limit = req.query.limit;
-	
+	var name =req.params.country;
+	var año =req.params.year;
+	var resul= [];
 	for ( var i=0; i < musical.length; i++) {
-		if(musical[i].name == name){
+		if(musical[i].country == name && musical[i].year == año){
 			console.log("New Get of resourse "+ name);
-			res.send(musical[i]);
-			break;
+			//res.send(musical[i]);
+			resul.push(musical[i]);		
 		}}
-	console.log("Get of music "+ name + "not found");
+
+	res.send(resul);	
+	console.log("Get of music "+ country + "not found");
+	res.sendStatus("Error 404");
+			//break;
+	
+};
+
+
+module.exports.getMusic2 = function(req,res){//FUNCIONA
+	var name =req.params.country;
+	//var limit = req.query.limit;
+	var resul= [];
+	for ( var i=0; i < musical.length; i++) {
+		if(musical[i].country == name){			
+			console.log("New Get of resourse "+ name);
+			//res.send(musical[i]);
+			resul.push(musical[i]);
+		}}
+	res.send(resul);
+	console.log("Get of music "+ country + "not found");
 	res.send("Error 404");
 			//break;
 	
@@ -42,12 +61,27 @@ module.exports.getMusic2 = function(req,res){
 };
 
 
+module.exports.getMusic4 = function(req,res){// ES IGUAL QUE COUNTRY (QUE SI FUNCIONA) Y YEAR NO
+	var year = req.params.year;
+	var resul = [];
+	for ( var i=0; i< musical.length; i++) {
+		if(musical[i].year == year){
+			console.log("New get of resource year" + year);
+			resul.push(musical[i]);
+		}}
+	res.send(resul);
+	console.log("Get of music4"+ year + "not found");
+	res.sendStatus("Errooooor");
+};
+
+
+
 
 module.exports.postMusic = function(req,res){ // crea y es de cliente a servidor
 
 	var mus= req.body; //recoge los datos
 	musical.push(mus);
-	console.log("New Post of music "+mus.name);
+	console.log("New Post of music "+mus);
 	res.sendStatus(200);
 };
 
@@ -67,21 +101,20 @@ module.exports.putMusic = function (req,res){
 
 module.exports.putMusic2 = function(req,res){
 
-	var name = req.params.name;
+	var country = req.params.country;
 	var mus = req.body;
 	for (var i = 0; i < musical.length; i++) {
 
-		if(musical[i].name = name){
 
-			musical[i].name = mus.name;
-			
-			console.log(" New Put of music "+name);
+		if(musical[i].country = country){
+			musical[i].country = mus.country;
+			console.log(" New Put of music "+ country);
 			res.send(200);
 			break;
 
 		}}	
 
-	console.log("Put of new "+name+" not found");
+	console.log("Put of new "+ country+" not found");
 	res.sendStatus(400);
 	//break;
 	
@@ -96,9 +129,9 @@ module.exports.deleteMusic = function(req,res)  {
 };
 
 module.exports.deleteMusic2 = function  (req,res)  {
-	var name = req.params.name;
+	var name = req.params.country;
 	for(var i=0;i<musical.length;i++){
-		if(musical[i].name == name){
+		if(musical[i].country == name){
 			console.log("New Delete of resource " + name);
 			musical.splice(i, 1);
 			res.sendStatus(200);
