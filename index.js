fd163621c8b1171630058f7aa06626804fc2318a
Blanca Hernández Laserna <hdzlaserna@gmail.com>
2016-04-04 09:58:12 +0200

    test v1. Closes #22

--- index.js
+++ index.js
@@ -7,21 +7,16 @@ var musicCtl = require('./controles/musicCTL.js');
 var app= express();
 var port = (process.env.PORT || 12345);
 
-<<<<<<< HEAD
 
-=======
->>>>>>> cf79c8ae193427a224ad57032c552dabcf6fb063
 var moviesCtl= require('./controles/moviesCtl.js');
 
 
 app.use("/",express.static(__dirname+"/static"));
 
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> cf79c8ae193427a224ad57032c552dabcf6fb063
+
 app.use(bodyParser.json());
 app.use("/",express.static(__dirname+"/static"));
 
@@ -37,130 +32,56 @@ app.get("/api/v1/music/:country",musicCtl.getMusic2); //funciona
 
 app.get("/api/v1/music/:country/:year",musicCtl.getMusic3);// funciona
 
-app.get("/api/v1/music/:country",musicCtl.getMusic4); //año
-
-<<<<<<< HEAD
-
-=======
->>>>>>> cf79c8ae193427a224ad57032c552dabcf6fb063
+app.get("/api/v1/music/:year",musicCtl.getMusic4); //año
 
 
-
-app.post("/api/v1/music", musicCtl.postMusic); //funciona
-app.post("/api/v1/music/:country",musicCtl.postMusic2);//funciona
+app.post("/api/v1/music", musicCtl.postMusic); 
+app.post("/api/v1/music/:country",musicCtl.postMusic2);
 
 
 
-app.put("/api/v1/music", musicCtl.putMusic);//funciona
-app.put("/api/v1/music/:country", musicCtl.putMusic2);//funciona
+app.put("/api/v1/music", musicCtl.putMusic);
+app.put("/api/v1/music/:country", musicCtl.putMusic2);
 
-app.delete("/api/v1/music", musicCtl.deleteMusic );//funciona
-app.delete("/api/v1/music/:country", musicCtl.deleteMusic2);//funciona
-
-
-
-////////////////////////////////////////////////////////
-/////////////////API CANDELA///////////////////////////
-
-<<<<<<< HEAD
+app.delete("/api/v1/music", musicCtl.deleteMusic );
+app.delete("/api/v1/music/:country", musicCtl.deleteMusic2);
 
 
 
-// app.get("/about/country-types",(req,res)=>{
-// 	fs.readFile('countrytypes.json','utf-8',(err,content)=>{
-// 		console.log("Data read");
-// 		countrytypes= JSON.parse(content);
-// 		res.write("<html><body> we can see the percentage of population listening music , besides the musical genre most listened <ul>");
-// 		countrytypes.forEach((linea)=>{
-// 			res.write("<li>"+linea.country+" , "+linea.year+" , "+linea.percentage+" , "+linea.type+"</li>");
-// 		});
-// 		res.write("</ul>------------------------------------------------------------------------</body></html>");
-// 		res.end();
 
-
-=======
-
->>>>>>> cf79c8ae193427a224ad57032c552dabcf6fb063
 
 app.get('/api/v1/social_situation/loadInitialData',moviesCtl.getLoad);
 app.get("/api/v1/social_situation",moviesCtl.getMovie);
 app.get("/api/v1/social_situation/:country",moviesCtl.getMovie2);
-<<<<<<< HEAD
-
-// 	});
-// });
-
-// app.get("/about/social-situation",(req,res)=>{
-// 	fs.readFile('socialsituation.json','utf-8',(err,content)=>{
-// 		console.log("Data read");
-// 		socialsituation= JSON.parse(content);
 
 
-// 		res.write("<html><body> Here we see the percentages according to digital music sales or physical format <ul>");
-// 		res.write("<table ><tr><td ><strong> country</strong></td>")
-// 		res.write("<td><strong> year</strong></td>")
-// 		res.write("<td><strong> sales</strong></td>")
-// 		res.write("<td><strong> digital</strong></td>")
-// 		res.write("<td><strong> noDigital</strong></td></tr></table>")
-		
-// 		socialsituation.forEach((linea)=>{
-// 			//res.write("<li>"+linea.country  +" , "+linea.year+" , "+linea.sales+" , "+linea.digital+", "+linea.nodigital+"</li>");
-// 			res.write("<table><tr><td>"+linea.country+"  "+linea.year+"  "+linea.sales+"  "+linea.digital+"  "+linea.nodigital+"</td></tr></table>")
-=======
-
->>>>>>> cf79c8ae193427a224ad57032c552dabcf6fb063
 
 app.get("/api/v1/social_situation/:country/:year",moviesCtl.getMovie3);
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> cf79c8ae193427a224ad57032c552dabcf6fb063
 app.post("/api/v1/social_situation",moviesCtl.postMovie); 
 app.post("/api/v1/social_situation/:country",moviesCtl.postMovie2); 
 app.put("/api/v1/social_situation",moviesCtl.putMovie); 
 
-<<<<<<< HEAD
-
-		
-
-// 	});
-// });
 
 
-//Gonzalo
+
+
+
 
 app.put('/api/v1/social_situation/:country',moviesCtl.putMovie2); 
 
 
 
-=======
+
 app.put('/api/v1/social_situation/:country',moviesCtl.putMovie2); 
 
->>>>>>> cf79c8ae193427a224ad57032c552dabcf6fb063
+
 app.delete("/api/v1/social_situation/:country" ,moviesCtl.deleteMovie);
 
-//Deletes all the resources in the given directory
 app.delete("/api/v1/social_situation",moviesCtl.deleteMovie2); 
 
-// app.get("/about/sex-and-ages",(req,res)=>{
-// 	fs.readFile('sex-and-ages.json','utf8',(err,content)=>{
-// 		console.log("Data read");
-// 		sexandages = JSON.parse(content);
-
-// 		res.write("<html><body> Statistics of musical tastes based on the sex and age <ul>");
-		
-// 		sexandages.forEach((linea)=>{
-// 			res.write(" - "+linea.sex+"  "+linea.country+" "+linea.year+" "+linea.POP+" "+linea.POProck+" "+linea.rap+" "+
-// 				linea.reggaeton+" "+linea.rock+" "+linea.classic+" "+linea.dance+" "+linea.flamenco+" "+linea.heavymetal+" "+linea.indie);
-// 		});
-
-// 		res.write("_____________");
-// 		res.end();
-// 	});
-// });
 
 
 
