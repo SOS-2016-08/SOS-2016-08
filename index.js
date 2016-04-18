48695fc97b5a6e444c7a7765e51357fd1a3fec45
Candela Madero <candela046@gmail.com>
2016-04-18 11:50:02 +0200

    changes

--- index.js
+++ index.js
@@ -37,28 +37,27 @@ app.post("/api/v1/music/:country",musicCtl.postMusic2);//funciona
 app.put("/api/v1/music", musicCtl.putMusic);//funciona
 app.put("/api/v1/music/:country/:year", musicCtl.putMusicCountryandYear);// funciona
 
-<<<<<<< HEAD
+
 ////////////////////////////CANDELA//////////////////////////////////////
-=======
 app.delete("/api/v1/music", musicCtl.deleteMusic );//funciona
 app.delete("/api/v1/music/:country/:year", musicCtl.deleteMusic2);//funciona
->>>>>>> 2d992353e8f3830664e3e27607ce6f06ac14c9e7
+
 
 //////get////
 
 
-<<<<<<< HEAD
+
 app.get("/api/v1/social_situation/loadInitialData",moviesCtl.getLoad);
-=======
+
 //--------- API MOVIES--------------------------
 app.get('/api/v1/social_situation/loadInitialData',moviesCtl.getLoad);
 
->>>>>>> 2d992353e8f3830664e3e27607ce6f06ac14c9e7
+
 app.get("/api/v1/social_situation",moviesCtl.getMovie);
 app.get("/api/v1/social_situation/:country",moviesCtl.getMovie2);
 app.get("/api/v1/social_situation/:country/:year",moviesCtl.getMovie3);
 
-<<<<<<< HEAD
+
 
 ////post////
 app.post("/api/v1/social_situation",moviesCtl.postMovie); 
@@ -68,34 +67,26 @@ app.post("/api/v1/social_situation/:country",moviesCtl.postMovie2);
 
 //put/////
 app.put("/api/v1/social_situation",moviesCtl.putMovie);
-=======
+
 app.post("/api/v1/social_situation",moviesCtl.postMovie); 
 app.post("/api/v1/social_situation/:country",moviesCtl.postMovie2); 
 
 app.put("/api/v1/social_situation",moviesCtl.putMovie); 
->>>>>>> 2d992353e8f3830664e3e27607ce6f06ac14c9e7
 app.put('/api/v1/social_situation/:country',moviesCtl.putMovie2); 
 app.put('/api/v1/social_situation/:country/:year',moviesCtl.putMovie2); 
 
-<<<<<<< HEAD
+
 
 
 ////delete
 app.delete("/api/v1/social_situation/:country/:year" ,moviesCtl.deleteMovie);
-=======
->>>>>>> 2d992353e8f3830664e3e27607ce6f06ac14c9e7
+
 app.delete("/api/v1/social_situation/:country" ,moviesCtl.deleteMovie);
 app.delete("/api/v1/social_situation",moviesCtl.deleteMovie2); 
 
 
 
-<<<<<<< HEAD
-=======
 
-
-
-
->>>>>>> 2d992353e8f3830664e3e27607ce6f06ac14c9e7
 
 
 
