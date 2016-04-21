d5c4e0e4aef6a7b5036cfe98a03e8fe54407dc5f
Blanca Hernández Laserna <hdzlaserna@gmail.com>
2016-04-21 16:35:19 +0200

    end of issue. Closed#28

--- controles/musicCTL.js
+++ controles/musicCTL.js
@@ -11,15 +11,7 @@ var musical=[
 
 var pass=123;
 
-/*module.exports.getLoad = function(req,res){ //FUNCIONA
-	musical=[];
-	var mus= fs.readFileSync('countrytypes.json', 'utf8');
-	musical = JSON.parse(mus);
-	res.send(musical);
-	res.sendStatus(200);
-	
 
-};*/
 
 module.exports.getLoad=function(req,res){
   //lee datos
@@ -39,52 +31,6 @@ module.exports.getLoad=function(req,res){
 };
 
 
-
-/*module.exports.getMusic = function(req,res){// FUNCIONA
-	console.log("New Get of music ");
-	res.status(200).jsonp(musical);
-};*/
-
-/*module.exports.getMusic=function(req,res){//get y limit general
-  fr = req.query.from;
-  to = req.query.to;
-  limit= req.query.limit;
-  offset= req.query.offset;
-  apikey= req.query.apikey;
-
-  var resul=[];
-
-  if(apikey==pass){
-
-    for (var i =0;i< musical.length ;  i++) {
-
-      resul.push(musical[i]);
-    }
-  
-    if ( fr && to){
-      for (var i=0; i<resul.length; i++){
-        if( resul[i].year < fr || resul[i].year > to){
-          resul.splice(i,1);
-          i=i-1;
-        }
-      }
-    }
-    if (limit && offset){
-      for (i=limit;i<offset;i++){
-        resul.push(musical[i]);
-
-      }
-      
-    }
-  res.send(resul);
-  }
-  else{
-
-    res.sendStatus(401);
-  }
-//res.send(resul);
-
-};*/
 
 
 
@@ -127,28 +73,6 @@ module.exports.getMusic=function(req,res){
   }
 };
 
-/*module.exports.getMusicCountryandYear = function(req,res){ 
-
-	var name =req.params.country;
-	var año =req.params.year;
-	var resul= [];
-	for ( var i=0; i < musical.length; i++) {
-		if(musical[i].country == name && musical[i].year == año){
-			console.log("New Get of resourse ");
-			//res.send(musical[i]);
-			resul.push(musical[i]);		
-		}
-	}
-	if( resul.length == 0){
-		res.sendStatus( 400);
-
-	}else{
-		res.send(resul);
-		res.sendStatus(200);
-
-	}
-	
-};*/
 
 module.exports.getMusicCountryandYear=function(req,res){ //get country/year
 	var name = req.params.country;
@@ -187,29 +111,7 @@ module.exports.getMusicCountryandYear=function(req,res){ //get country/year
 };
 
 
-/*module.exports.getMusicCountryorYear = function(req,res){
-	var valor = req.params.valor;
-	var resul = [];
 
-
-
-	for ( var i=0; i< musical.length; i++) {
-		if(musical[i].year == valor){
-			console.log("New get of resource year" );
-			resul.push(musical[i]);
-		}else if( musical[i].country == valor){
-			console.log("New get of resource country" );
-			resul.push(musical[i]);
-
-		}
-	}
-	if(resul.length == 0){
-		res.sendStatus(400);
-	}else{
-		res.send(resul);
-		res.sendStatus(200);
-	}
-};*/
 
 module.exports.getMusicCountryorYear=function(req,res){ //get name or get year
 	var valor = req.params.valor;
@@ -244,27 +146,7 @@ module.exports.getMusicCountryorYear=function(req,res){ //get name or get year
 
 
 
-/*module.exports.postMusic = function(req,res){ // crea y es de cliente a servidor
 
-	var mus= req.body; //recoge los datos
-	musical.push(mus);
-	console.log("New Post of music ");
-	res.sendStatus(200);
-};
-*/
-
-/*module.exports.postMusic=function(req,res){
-	var apikey=req.query.apikey;
-	if(apikey==pass){
-		var mov= req.body;
-		movies.push(mov);
-		console.log("New post"+mov.name);
-		res.sendStatus(201);
-  	}else{
-  		res.sendStatus(401);
-  	}
-
-};*/
 
 module.exports.postMusic=function(req,res){
 	var apikey = req.query.apikey;
@@ -293,20 +175,6 @@ module.exports.postMusic=function(req,res){
 
 
 
-
-
-
-
-
-
-
-/*module.exports.postMusic2 = function (req,res){ 
-
-	console.log("The Post of music is not possible");
-	//res.send("Error 405 : Method not allowed");
-	res.sendStatus(400);
-};*/
-
 module.exports.postMusic2=function(req,res){
 	var apikey=req.query.apikey;
 	if(apikey==pass){
@@ -327,11 +195,7 @@ function PosArray(str,elements){
     return acum;
 };
 
-/*module.exports.putMusic = function (req,res){
 
-	console.log("The Put of music is not possible");
-	res.sendStatus(400);//dfghjkl
-};*/
 
 module.exports.putMusic=function(req,res){
 	var apikey=req.query.apikey;
@@ -346,69 +210,7 @@ module.exports.putMusic=function(req,res){
 
 
 
-/*module.exports.putMusicYearandCountry = function(req,res){
-	var country = req.params.country;
-	var year = req.params.year;
- 	var nuevo = req.body;
-  for (var i=0; i<=musical.length;i++){
 
-    if(musical[i].country == country && musical[i].year == year){
-      musical.splice(i, 1);
-      musical.push(nuevo);
-      res.sendStatus(200);
-      break;
-    }
-  }
-  res.sendStatus(400);
-
-};*/
-/*function CheckBody(body){
-    return body.country && body.year && body.percentage && body.type;
-    
-}
-
-
-
-function validar(str1,str2,elements){
-  var cont = -1;
-  for(var i=0;i<elements.length;i++)
-    if(elements[i].country==str1 && elements[i].year==str2){
-      cont=i;
-    }
-  return cont;
-};*/
-
-/*module.exports.putMusicCountryandYear=function (req,res){ 
-  var apikey=req.query.apikey;
-  if(apikey==pass){
-    console.log(req.body);
-    console.log(req.params.country);
-    console.log(req.params.year);
-    if(CheckBody(req.body) && req.body.country==req.params.country && req.body.year==req.params.year){
-        var si = validar(req.params.country,req.params.year,musical);
-        console.log("valor de si",si);//req url
-          if (si != -1){
-            musical[si].country=req.body.country;//req body
-            musical[si].year=req.body.year;
-            musical[si].percentage=req.body.percentage;
-            musical[si].type=req.body.type;
-            
-            res.sendStatus(200);//ok
-          }else{
-          res.sendStatus(400);//Not Found
-          }
-    }else{
-      res.sendStatus(404);//bad request
-    }
-      
-  }else{
-    console.log("you must identificate");
-    res.sendStatus(401);//Unauthorized
-  }
-    
-};
-
-*/
 module.exports.putMusicCountryandYear = function(req, res){
   var country = req.params.country;
   var year = req.params.year;
@@ -452,11 +254,7 @@ module.exports.putMusicCountryandYear = function(req, res){
 };
 
 
-/*module.exports.deleteMusic = function(req,res)  {
-	console.log("New Delete all resources");
-	musical.splice(musical);
-	res.sendStatus(200);
-};*/
+
 
 module.exports.deleteMusicRecurso=function(req,res){
 
@@ -490,22 +288,7 @@ module.exports.deleteMusicRecurso=function(req,res){
 
   }
 };
-/*module.exports.deleteMusic2 = function  (req,res)  {
-	var name = req.params.country;
-	for(var i=0;i<musical.length;i++){
-		if(musical[i].country == name){
-			console.log("New Delete of resource " + name);
-			musical.splice(i, 1);
-			res.sendStatus(200);
-      		break;
 
-    	}}
-    console.log("Delete of music " + name + " no found");
-    res.sendStatus( 400);
-    //break;
-    
- 
-};*/
 module.exports.deleteMusic=function(req,res){
 	var apikey=req.query.apikey;
 	if(apikey==pass){
