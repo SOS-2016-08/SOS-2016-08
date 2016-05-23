588777a58a7ed320b563bae2a87e8b18f43718ee
Blanca Hernández Laserna <hdzlaserna@gmail.com>
2016-05-23 16:14:18 +0200

    end. Closes #38

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get("/api/prueba", (req,res)=>{
 });
 
 
-<<<<<<< HEAD
+
 
 //multiPlan_C2_sos-2016-08-cmg_ag
 
@@ -90,7 +90,7 @@ governify.control(app,{
   defaultPath :"/api/v1/social_situation"
 });
 
-=======
+
 governify.control(app,{
   datastore:"http://datastore.governify.io/api/v6.1/",
   namespace: "sos-2016-08-bhl",
@@ -102,7 +102,7 @@ var port = (process.env.PORT || 12345);
 app.use("/",express.static(__dirname+"/static"));
 app.use(bodyParser.json());
 
->>>>>>> 6d1900847fa4754e18a785db3a582e61629d61d1
+
 
 app.use(cors());
 var musicCtl = require('./controles/musicCTL.js');
